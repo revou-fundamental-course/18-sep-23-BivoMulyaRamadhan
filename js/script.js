fbb1aff4a5f3cb2c1fa7441775888dad266bf55b
Bivo Mulya Ramadhan <bivomulyaramadhan24@gmail.com>
2023-09-28 23:33:46 +0700

    change js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.getElementById("button").addEventListener("click", function() {
     let name = document.forms["contact-form"]["your-name"].value;
-    let email = document.forms["contact-form"]["email"].value;
+    let email = document.forms["contact-form"]["your-email"].value;
     let interest = document.forms["contact-form"]["interest"].value;
     
     if (name == "") {
@@ -22,20 +22,4 @@ document.getElementById("button").addEventListener("click", function() {
     }
 
 
-});
-
-const carouselSlides = document.querySelectorAll(".image");
-let currentIndex = 0;
-let interval = 3000;
-
-
-function nextSLide() {
-    carouselSlides[currentIndex].classList.remove("active");
-    currentIndex = (currentIndex + 1) % carouselSlides.length;
-    carouselSlides[currentIndex].classList.add("active");
-}
-
-
-(function() {
-    setInterval(nextSLide, interval);
-})()
+})
